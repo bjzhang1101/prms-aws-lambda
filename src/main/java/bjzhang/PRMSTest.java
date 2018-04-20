4099c7bd970eb3b3fba511b21d6640545438a2cb
BufferBoga <bjzhang@uw.edu>
2018-04-19 23:51:28 -0700

    code runs well, upload problem remained

--- src/main/java/bjzhang/PRMSTest.java
+++ src/main/java/bjzhang/PRMSTest.java
@@ -5,9 +5,6 @@
  */
 package bjzhang;
 
-import java.io.*;
-import java.util.*;
-
 import com.amazonaws.services.lambda.runtime.ClientContext;
 import com.amazonaws.services.lambda.runtime.CognitoIdentity;
 import com.amazonaws.services.lambda.runtime.Context; 
@@ -17,9 +14,12 @@ import com.amazonaws.services.s3.model.*;
 import com.amazonaws.services.s3.*;
 import com.amazonaws.services.s3.transfer.*;
 import com.amazonaws.AmazonServiceException;
+import com.amazonaws.util.IOUtils;
 import java.nio.charset.StandardCharsets;
 import java.nio.file.*;
 import java.util.stream.Collectors;
+import java.io.*;
+import java.util.*;
 
 
 /**
@@ -32,119 +32,95 @@ public class PRMSTest implements RequestHandler<Request, Response>{
      * @param args the command line arguments
      */
     public Response handleRequest(Request request, Context context){
+        
+        
+        
+        ////////////////////////////////////////////////////////////////////////
+        /////////////////////     pull data from S3     ////////////////////////
+        ////////////////////////////////////////////////////////////////////////
         String bucket_input = "zumboboga";
         String bucket_output = "zumbobogaresized";
+        
         AmazonS3 client = new AmazonS3Client();
-        S3Object xFile = client.getObject(bucket_input, "data.csv");
+
+        //input data.csv
+        S3Object x1 = client.getObject(bucket_input, "data.csv");
+        InputStream contents = x1.getObjectContent();
         
-        
-        
-        InputStream contents = xFile.getObjectContent();
-        
+        /*
+        // upload files by string
         String tmp = new BufferedReader(new InputStreamReader(contents))
                 .lines().collect(Collectors.joining(System.lineSeparator()));
         
-        //upload by string
-        client.putObject(bucket_output, "2.csv", tmp);
-        
-        
-        File file = new File("/tmp");
-        File file_dir = new File("/tmp/dir");
-        file_dir.mkdirs();
-        File f2 = new File("/tmp/dir/1.txt");
+        client.putObject(bucket_output, "data.csv", tmp);
+        */
+       
+        File f1 = new File("/tmp/data.csv");
         try{
-            f2.createNewFile();
-            BufferedWriter out = new BufferedWriter(new FileWriter(f2));
-            out.write("hi\r\n");
-            out.flush();
-            out.close();
-        } catch(IOException e){
-            e.printStackTrace();
-        }
-        
-        File f3 = new File("/tmp/dir/3.txt");
-        try{
-            f3.createNewFile();
-            BufferedWriter out = new BufferedWriter(new FileWriter(f3));
-            out.write("hihi\r\n");
-            out.flush();
-            out.close();
-        } catch(IOException e){
-            e.printStackTrace();
-        }
-        
-        try{
-            Thread.sleep(150000);
-        } catch (InterruptedException i){
+            Files.copy(contents, f1.toPath());
+        } catch (IOException i){
             i.printStackTrace();
         }
-        /*
-        File f = new File("/tmp");
-        Path p = Paths.get("/tmp/test");
         
-        try (BufferedWriter bw = Files.newBufferedWriter(p, StandardCharsets.US_ASCII, StandardOpenOption.CREATE_NEW))
-            {
-                bw.write("hello");
-                bw.close();
-            }
-            catch (IOException ioe)
-            {
-                ioe.printStackTrace();
-            }
-        */
-        /*
-        Path p1 = Paths.get("/tmp/test1");
+        //input mixed_params.csv
+        S3Object x2 = client.getObject(bucket_input, "mixed_params.csv");
+        InputStream contents2 = x2.getObjectContent();
         
-        try (BufferedWriter bw = Files.newBufferedWriter(p1, StandardCharsets.US_ASCII, StandardOpenOption.CREATE_NEW))
-            {
-                bw.write("hello");
-                bw.close();
-            }
-            catch (IOException ioe)
-            {
-                ioe.printStackTrace();
-            }
-        */
-        /*
-        client.putObject(bucket_output, tmp, file);
-        */
+        File f2 = new File("/tmp/mixed_params.csv");
+        try{
+            Files.copy(contents2, f2.toPath());
+        } catch (IOException i){
+            i.printStackTrace();
+        }
 
-        //File f1 = new File("/tmp");
-        //TransferManager trans = new TransferManager();
+        //input efcarson.sim
+        S3Object x3 = client.getObject(bucket_input, "efcarson.sim");
+        InputStream contents3 = x3.getObjectContent();
+        
+        File f3 = new File("/tmp/efcarson.sim");
+        try{
+            Files.copy(contents3, f3.toPath());
+        } catch (IOException i){
+            i.printStackTrace();
+        }
+        
+        ////////////////////////////////////////////////////////////////////////
+        //////////////////           Run Project        ////////////////////////
+        ////////////////////////////////////////////////////////////////////////
+        // PRMS project home dir (Change this path to reflect your path to "prmsprj")
+        String prj = "/tmp";
+
+        // Pass the project home to OMS
+        System.setProperty("oms_prj", prj);
+
+        // runs the sim file via OMS CLI in a certain logging level
+        oms3.CLI.main(new String[]{
+            "-l", "OFF", // can be "OFF", "ALL", .... (all JDK loging levels)
+            "-r", prj + "/efcarson.sim" // runs the sim file in project home.
+        });
+        
+        
+        
+        ////////////////////////////////////////////////////////////////////////
+        ////////           Upload the output Directory to S3        ////////////
+        ////////////////////////////////////////////////////////////////////////
         TransferManager trans = TransferManagerBuilder.standard().build();
-        MultipleFileUpload xfer = trans.uploadDirectory(bucket_output, null, file, true);
+        MultipleFileUpload xfer = trans.uploadDirectory(bucket_output, null
+                , new File("/tmp"), true);
         try{
             xfer.waitForCompletion();
         } catch (InterruptedException i){
             i.printStackTrace();
         }
-        //xfer.waitForCompletion();
         
-        //trans.uploadDirectory(bucket_output, "", file, false);
-        
-        //upload by file
-        /*
-        File file = null;
-        byte[] buf = new byte[1024];
-        try{
-            OutputStream out = new FileOutputStream(file);
-            out.write(buf, 0, 10);
-        }catch (Exception e){
-            e.getStackTrace();
-        }
-        client.putObject(bucket_output, "2.txt", file);
-        */
-        File ff = new File("");
-        
-        int num = 0;
-        //for(String str : file.list()) ++num;
-
-        //String s = p.toString();
-        
+        ////////////////////////////////////////////////////////////////////////
+        //     Response
+        ////////////////////////////////////////////////////////////////////////
         //int age = request.calcs;
-        int age = num;
+        int age = 0;
         //String name = tmp + "!!!!!!";
-        String name = ff.getAbsolutePath();
+        //String name = file.getAbsolutePath();
+        String name = request.base;
         String Base64 = request.base;
         Response res = new Response(name, age, Base64);
         return res;
